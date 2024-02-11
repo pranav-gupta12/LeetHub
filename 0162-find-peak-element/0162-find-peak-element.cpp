12d9880c9054c9bbfd2787927e16512eb4683467
pranav-gupta12 <136360780+pranav-gupta12@users.noreply.github.com>
2024-02-11 15:50:14 +0530

    Time: 0 ms (100.00%), Space: 11.2 MB (31.08%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
-    int findPeakElement(vector<int>& nums) {
-        int n = nums.size();
-          
-        if(n==1) return 0;
-         if(nums[0]>nums[1]) return 0;
-            if(nums[n-1]>nums[n-2]) return n-1;
-        int s=1,e=n-2;
-        while(s<=e)
-        {
-         
-            int mid = (s+e)>>1;
-            if(nums[mid]>nums[mid+1] && nums[mid]> nums[mid-1]) return mid;
-            else if (nums[mid]>nums[mid-1]) s = mid+1;
-            else e = mid-1;
-        }
-        return -1;
+    int findPeakElement(vector<int>& arr) {
+         int n = arr.size(); //Size of array.
+
+    // Edge cases:
+    if (n == 1) return 0;
+    if (arr[0] > arr[1]) return 0;
+    if (arr[n - 1] > arr[n - 2]) return n - 1;
+
+    int low = 1, high = n - 2;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        //If arr[mid] is the peak:
+        if (arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1])
+            return mid;
+
+        // If we are in the left:
+        if (arr[mid] > arr[mid - 1]) low = mid + 1;
+
+        // If we are in the right:
+        // Or, arr[mid] is a common point:
+        else high = mid - 1;
+    }
+    // Dummy return statement
+    return -1;
     }
 };
