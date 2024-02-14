7372a5ab79b5eb9268e03705e329d3769cb665e0
pranav-gupta12 <136360780+pranav-gupta12@users.noreply.github.com>
2024-02-14 20:16:00 +0530

    Time: 111 ms (84.75%), Space: 69 MB (35.91%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,51 +1,38 @@
 class Solution {
+    
 public:
-
-    int possibleBouquets(vector<int> &bloomDay, int n, int k, long long mid) {
-
-        int bouquets = 0, adjCount = 0;
-
-        for(int i = 0; i < n; i++) {
-            if(bloomDay[i] <= mid) {
-                adjCount++;
+    bool IsPossible(vector<int>& arr, int m, int k, int mid)
+    {
+       int ans = 0;
+    int count = 0;
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr[i] <= mid) {
+            count++;
+            if (count == k) {
+                ans++;
+                if (ans == m) return true;
+                count = 0;
             }
-            else {
-                bouquets += (adjCount / k);
-                adjCount = 0;
-            }
-        } 
-
-        bouquets += (adjCount / k);
-
-        return bouquets;
-
+        } else {
+            count = 0;
+        }
     }
-
-    int minDays(vector<int>& bloomDay, int m, int k) {
-        
-        int n = bloomDay.size();
-
-        if(n < (long long)m * k)
-            return -1;
-
-        long long low = 1, high = *max_element(bloomDay.begin(), bloomDay.end());
-        long long ans = high;
-
-        while(low <= high) {
-            long long mid = (low + high) >> 1;
-
-            int bouquets = possibleBouquets(bloomDay, n, k, mid);
-
-            if(bouquets >= m) {
-                ans = min(ans, mid);
+    return false;
+    }
+    int minDays(vector<int>& arr, int m, int k) {
+         if ((long long)m * k > arr.size()) return -1;
+       int low =  *min_element (arr.begin(), arr.end());
+int high = *max_element (arr.begin(), arr.end());
+        int ans = -1;
+        while(low<=high)
+        {
+            int mid = (low+high)>>1;
+            if(IsPossible(arr, m,k,mid)) {
+                ans = mid;
                 high = mid - 1;
             }
-            else {
-                low = mid + 1;
-            }
-        }
-
+            else  low = mid+1;
+        } 
         return ans;
-
     }
 };
