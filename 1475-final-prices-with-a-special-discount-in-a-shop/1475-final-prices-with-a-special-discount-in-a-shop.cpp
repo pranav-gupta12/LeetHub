0e0e794af173f3ce75a1f3a0ae5c04c206579aa3
pranav-gupta12 <136360780+pranav-gupta12@users.noreply.github.com>
2024-02-21 23:19:11 +0530

    Time: 0 ms (100.00%), Space: 12.4 MB (36.01%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -4,19 +4,16 @@ public:
         int n = prices.size();
         vector<int> ans(n,0);
         stack<int> s;
-        // s.push(0);
+        s.push(0);
         for(int i=n-1;i>=0;i--)
         {
-           int curr = prices[i];
-    while(!s.empty() && curr < s.top()) {
-        s.pop();
-    }
-    if (!s.empty()) {
-        ans[i] = curr - s.top();
-    } else {
-        ans[i] = curr; // If the stack is empty, the discount cannot be applied
-    }
-    s.push(curr);
+            int curr = prices[i];
+            while(curr <s.top() )
+            {
+                s.pop();
+            }
+            ans[i]= curr - s.top();
+            s.push(curr);
         }
         return ans;
         
