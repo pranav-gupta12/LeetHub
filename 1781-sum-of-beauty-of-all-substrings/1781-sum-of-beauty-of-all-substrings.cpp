0008f8143fb253ecb4a828e329d347dac728e9d6
pranav-gupta12 <136360780+pranav-gupta12@users.noreply.github.com>
2024-02-25 11:35:57 +0530

    Time: 94 ms (90.60%), Space: 9.9 MB (77.52%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -1,23 +1,34 @@
 class Solution {
 public:
-    int beauty(vector<int> array){
-        int mf=0;
-        int lf=INT_MAX;
-        for(int x : array){
-            mf = max(mf,x);
-            if(x>0) lf = min(lf,x);
+   int minimum(vector<int> &mp){
+        int k = INT_MAX;
+        for(auto it : mp){
+            if(it<=k && it>0)
+                k = it;
         }
-        return ( mf-lf);
+        return k;
+    }
+    int maximum(vector<int> &mp){
+        int k = INT_MIN;
+        for(auto it : mp){
+            if(it>=k)
+                k = it;
+        }
+        return k;
     }
     int beautySum(string s) {
-         int res = 0;
-        for(int i=0;i<s.length();i++){
-            vector<int> count(26,0);
-            for(int j=i;j<s.length();j++){
-                count[s[j]- 'a']++;
-                res+= beauty(count);
+        int sum = 0;
+        for(int i = 0; i<s.size();i++){
+            vector<int> mp(26,0);
+            for(int j=i; j<s.size();j++){
+                mp[s[j]-'a']++;
+                int mx = maximum(mp);
+                int mn = minimum(mp);
+                sum += mx - mn;
             }
         }
-        return res;
+        return sum;
+    
+        
     }
 };
