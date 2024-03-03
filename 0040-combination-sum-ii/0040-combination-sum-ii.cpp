082d1b6c53da7cf2e5eeecc63a3ee804c11756fa
pranav-gupta12 <136360780+pranav-gupta12@users.noreply.github.com>
2024-03-04 01:27:32 +0530

    Time: 0 ms (100.00%), Space: 12.7 MB (30.84%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,24 +1,24 @@
 class Solution {
     
 public:
-   void findCombination(int ind, int target, vector < int > & arr, vector < vector < int >> & ans, vector < int > & ds) {
-  if (target == 0) {
-    ans.push_back(ds);
-    return;
-  }
-  for (int i = ind; i < arr.size(); i++) {
-    if (i > ind && arr[i] == arr[i - 1]) continue;
-    if (arr[i] > target) break;
-    ds.push_back(arr[i]);
-    findCombination(i + 1, target - arr[i], arr, ans, ds);
-    ds.pop_back();
-  }
-}
+    void solve( int target,vector<int>& arr, vector<vector<int>> &ans, vector<int> &output, int index){
+        if(target==0){
+            ans.push_back(output);
+            return;
+        }
+        for(int i=index;i<arr.size();i++){
+            if(arr[i]>target) break;
+            if(i>index && arr[i-1]==arr[i]) continue;
+            output.push_back(arr[i]);
+            solve(target-arr[i],arr,ans,output,i+1);
+            output.pop_back();
+        }
+    }
     vector<vector<int>> combinationSum2(vector<int>& arr, int target) {
-        sort(arr.begin(), arr.end());
-  vector < vector < int >> ans;
-  vector < int > ds;
-  findCombination(0, target, arr, ans, ds);
-  return ans;
+        vector<vector<int>> ans;
+        sort(arr.begin(),arr.end());
+        vector<int> output;
+        solve(target,arr,ans,output,0);
+        return ans;
     }
 };
