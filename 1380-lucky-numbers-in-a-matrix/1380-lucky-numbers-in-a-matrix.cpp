9eb427aa684b8cc6188aab998a09bf19caad2aa9
pranav-gupta12 <136360780+pranav-gupta12@users.noreply.github.com>
2024-07-19 18:38:22 +0530

    Time: 20 ms (23.27%), Space: 14.3 MB (18.75%) - LeetHub

--- 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.cpp
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.cpp
@@ -3,27 +3,30 @@ public:
     vector<int> luckyNumbers (vector<vector<int>>& matrix) {
         int row = matrix.size();
         int col = matrix[0].size();
-        vector<int> minrow(row,0);
-        unordered_set<int> maxcol;
+       vector<int> ans;
         for(int i=0;i<row;i++){
-            int mini = INT_MAX;
+            pair<int,int> mini = {INT_MAX,-1};
             for(int j=0;j<col;j++){
-                mini = min(mini,matrix[i][j]);
+                // mini = min(mini,matrix[i][j]);
+                if(mini.first> matrix[i][j] ) {
+                    mini.first = matrix[i][j];
+                    mini.second = j;
+                }
             }
-            minrow.push_back(mini);
-        }
-        for(int i=0;i<col;i++){
-            int maxi = INT_MIN;
-            for(int j=0;j<row;j++){
-                maxi = max(maxi,matrix[j][i]);
-
+           int val = mini.first;
+            int idx = mini.second;
+            bool possible = true;
+            
+        for(int k=0;k<row;k++){
+            if(matrix[k][idx] > val){
+                possible = false;
+                break;
             }
-maxcol.insert(maxi);
+            
         }
-        vector<int> ans;
-        for(auto it : minrow){
-            if(maxcol.count(it)) ans.push_back(it);
+            if(possible) ans.push_back(val);
         }
+       
         return ans;
     }
 };
