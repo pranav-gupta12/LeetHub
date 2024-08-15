720eefd1d2b6f0215ad1057ea8affe1b7a0036f0
pranav-gupta12 <136360780+pranav-gupta12@users.noreply.github.com>
2024-08-16 03:57:20 +0530

    Time: 200 ms (57.15%), Space: 81.2 MB (73.44%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
@@ -1,12 +1,13 @@
 class Solution {
-    private:
-      static bool sortcol( vector<int> v1, const vector<int> v2)
+
+
+public:
+          static bool sortcol( vector<int>& v1,  vector<int>& v2)
 {
           if(v1[0]==v2[0]) return v1[1] > v2[1];
     return v1[0] < v2[0];
 }
-public:
-    // bool sortcol(const vector<int>& v1,const vector<int>& v2) {return v1[0]<v2[0];}
+
  
     int binarydp(vector<vector<int>>& envelopes){
         vector<int> ans;
