7b2aa740c459282b4b7b3d169be6e70ecef24796
pranav-gupta12 <136360780+pranav-gupta12@users.noreply.github.com>
2024-09-05 17:24:01 +0530

    Time: 9 ms (26.23%), Space: 17 MB (61.62%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     void dfs(vector<vector<int>>& image, int sr, int sc, int color,int temp){
-        if(sr<0 || sr>=image.size() || sc<0 || sc>=image[0].size()) return;
-       if(image[sr][sc]==temp) image[sr][sc] = color;
-        else return;
+        if(sr<0 || sr>=image.size() || sc<0 || sc>=image[0].size() || image[sr][sc]!=temp) return;
+      image[sr][sc] = color;
+     
       dfs(image,sr+1,sc,color,temp);
          dfs(image,sr,sc+1,color,temp);
          dfs(image,sr-1,sc,color,temp);
