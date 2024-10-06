76060e0ca26df815f0409cf1a600997def23ca32
pranav-gupta12 <136360780+pranav-gupta12@users.noreply.github.com>
2024-10-07 01:53:03 +0530

    Time: 10 ms (40.88%), Space: 8.8 MB (75.25%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int n = s1.length();
+int m = s2.length();
+
+if (n > m) return false;
+
+vector<int> s1_freq(26, 0);
+vector<int> s2_freq(26, 0);
+
+for (int i = 0; i < n; i++) {
+    s1_freq[s1[i] - 'a']++;
+}
+
+int i = 0;
+int j = 0;
+while (j < m) {
+    s2_freq[s2[j] - 'a']++;
+
+    if (j - i + 1 > n) {
+        s2_freq[s2[i] - 'a']--;
+        i++;
+    }
+
+    if (s1_freq == s2_freq) {
+        return true;
+    }
+
+    j++;
+}
+
+return false;
+    }
+};
