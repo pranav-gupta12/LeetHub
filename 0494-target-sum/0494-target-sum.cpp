d42c0c8951fd6214dfedc645f33dee82f4ca0532
pranav-gupta12 <136360780+pranav-gupta12@users.noreply.github.com>
2024-12-26 13:08:18 +0530

    Time: 278 ms (36.82%), Space: 62.1 MB (10.54%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
-    int solve(vector<int>& nums,int idx,int sum, int& target){
+    int solve(vector<int>& nums,int idx,int sum, int& target, map<pair<int,int>, int>& memo){
         if( idx ==nums.size()) { if(target==sum) return 1;
                                  else return 0;}
-        int add = solve(nums,idx+1,sum+nums[idx],target);
-          int minus = solve(nums,idx+1,sum-nums[idx],target);
-        return add + minus;
+        pair<int,int> p = {idx,sum};
+        if(memo.find(p)!=memo.end()) return memo[{idx,sum}];
+        int add = solve(nums,idx+1,sum+nums[idx],target,memo);
+          int minus = solve(nums,idx+1,sum-nums[idx],target,memo);
+        return memo[p] = add+minus;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        return solve(nums,0,0,target); //arr,idx,sum,target
+        map<pair<int,int>, int> memo;
+      return solve(nums, 0, 0, target,memo);
     }
 };
